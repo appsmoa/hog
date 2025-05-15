d6573e9efd2824bdebacfa5c70cbf0d733a70b15
appsmoa <wildbear@nate.com>
2025-05-15 15:00:49 +0900

    5/22 commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // React 18 이상에서는 react-dom/client를 사용
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // createRoot 사용
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename="/hog">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
